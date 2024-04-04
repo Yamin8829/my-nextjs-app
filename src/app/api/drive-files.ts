b5e4ed74dc34a2ea6da132c5b54cf718c5176d5e
parapejuang <147990337+parapejuang@users.noreply.github.com>
2024-04-04 22:00:16 +0700

    benerin imgDrive files

--- src/app/api/drive-files.ts
+++ src/app/api/drive-files.ts
@@ -1,47 +1,47 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { google } from "googleapis";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { google } from "googleapis";
 
-export type DriveFile = {
-  kind: string;
-  id: string;
-  name: string;
-  mimeType: "image/jpeg";
-};
+// export type DriveFile = {
+//   kind: string;
+//   id: string;
+//   name: string;
+//   mimeType: "image/jpeg";
+// };
 
-export type Data = {
-  files: DriveFile[];
-};
+// export type Data = {
+//   files: DriveFile[];
+// };
 
-const CLIENT_ID =
-  "522559441441-abp60hcpnqe3t7962vbjetfgt1n3r4k.apps.googleusercontent.com";
-const CLIENT_SECRET = "GOCSPX-YNyOYJbxBPUBK60IZYjX5agcvfe";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+// const CLIENT_ID =
+//   "522559441441-abp60hcpnqe3t7962vbjetfgt1n3r4k.apps.googleusercontent.com";
+// const CLIENT_SECRET = "GOCSPX-YNyOYJbxBPUBK60IZYjX5agcvfe";
+// const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
-export default async function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  try {
-    const oauth2Client = new google.auth.OAuth2(
-      CLIENT_ID,
-      CLIENT_SECRET,
-      REDIRECT_URI
-    );
+// export default async function handler(
+//   _req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   try {
+//     const oauth2Client = new google.auth.OAuth2(
+//       CLIENT_ID,
+//       CLIENT_SECRET,
+//       REDIRECT_URI
+//     );
 
-    oauth2Client.setCredentials({
-      refresh_token:
-        "1//04rY5fFH0FlFlCgYIARAAGAQSNwF-L9Irs698cHcblPX7eZeDTPATx2nij6oH-r1Cutp8ceNgxNkyNMupvHHtUzkYxCJaEE4PWc",
-    });
+//     oauth2Client.setCredentials({
+//       refresh_token:
+//         "1//04rY5fFH0FlFlCgYIARAAGAQSNwF-L9Irs698cHcblPX7eZeDTPATx2nij6oH-r1Cutp8ceNgxNkyNMupvHHtUzkYxCJaEE4PWc",
+//     });
 
-    const drive = google.drive({
-      version: "v3",
-      auth: oauth2Client,
-    });
+//     const drive = google.drive({
+//       version: "v3",
+//       auth: oauth2Client,
+//     });
 
-    const response = await drive.files.list();
+//     const response = await drive.files.list();
 
-    res.status(200).json({ files: response.data.files as DriveFile[] });
-  } catch (error) {
-    console.error("Error fetching Google Drive files:");
-  }
-}
+//     res.status(200).json({ files: response.data.files as DriveFile[] });
+//   } catch (error) {
+//     console.error("Error fetching Google Drive files:");
+//   }
+// }
--- src/components/imgDrive.tsx
+++ src/components/imgDrive.tsx
@@ -1,41 +1,41 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import axios from "axios";
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import Image from "next/image";
+// import axios from "axios";
 
-const ImgDrive: React.FC = () => {
-  const [imageUrls, setImageUrls] = useState<string[]>([]);
+// const ImgDrive: React.FC = () => {
+//   const [imageUrls, setImageUrls] = useState<string[]>([]);
 
-  useEffect(() => {
-    const fetchImageUrls = async () => {
-      try {
-        const response = await axios.get(
-          "/google-img?folderId=1ZMtWX9kGEVFhA91d-TUjgv-eC3J3Jh7l"
-        );
-        setImageUrls(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+//   useEffect(() => {
+//     const fetchImageUrls = async () => {
+//       try {
+//         const response = await axios.get(
+//           "/google-img?folderId=1ZMtWX9kGEVFhA91d-TUjgv-eC3J3Jh7l"
+//         );
+//         setImageUrls(response.data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
 
-    fetchImageUrls();
-  }, []);
+//     fetchImageUrls();
+//   }, []);
 
-  return (
-    <div>
-      <h1>Image Drive Gallery</h1>
-      {imageUrls.map((url, index) => (
-        <div key={index}>
-          <Image
-            src={url}
-            alt={`Image ${index + 1}`}
-            width={300}
-            height={200}
-          />
-        </div>
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Image Drive Gallery</h1>
+//       {imageUrls.map((url, index) => (
+//         <div key={index}>
+//           <Image
+//             src={url}
+//             alt={`Image ${index + 1}`}
+//             width={300}
+//             height={200}
+//           />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default ImgDrive;
+// export default ImgDrive;
